d8d41dd1f00c2708e0f0f148753d43e28caf5d4b
Michael Silvanovich <silvanovich.michael@gmail.com>
2017-12-23 01:59:44 +0300

    refactoring: stub MUST be a stub

--- tests/src/test/java/ru/hardcoders/demo/webflux/web_handler/filters/logging/WithMemoizingFunction.java
+++ tests/src/test/java/ru/hardcoders/demo/webflux/web_handler/filters/logging/WithMemoizingFunction.java
@@ -5,12 +5,30 @@ import org.springframework.core.io.buffer.DataBuffer;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.channels.Channels;
 import java.util.function.Function;
 
 interface WithMemoizingFunction {
 
-    ByteArrayOutputStream EMPTY_BYTE_ARRAY_OUTPUT_STREAM = new ByteArrayOutputStream(0);
+    ByteArrayOutputStream EMPTY_BYTE_ARRAY_OUTPUT_STREAM = new ByteArrayOutputStream(0) {
+
+        @Override
+        public synchronized void write(int b) {
+            throw new UnsupportedOperationException("stub");
+        }
+
+        @Override
+        public synchronized void write(byte[] b, int off, int len) {
+            throw new UnsupportedOperationException("stub");
+        }
+
+        @Override
+        public synchronized void writeTo(OutputStream out) throws IOException {
+            throw new UnsupportedOperationException("stub");
+        }
+
+    };
 
     Logger getLogger();
 
